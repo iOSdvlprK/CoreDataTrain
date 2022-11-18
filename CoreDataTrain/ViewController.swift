821dc1f59ab6b4841d5154dcee1cb1968f757e92
iOS developer K <iOSdvlprK@gmail.com>
2022-11-19 01:32:23 +0900

    table view setup and UIColor extension

--- CoreDataTrain/ViewController.swift
+++ CoreDataTrain/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,6 +15,13 @@ class ViewController: UIViewController {
         view.backgroundColor = .white
         
         navigationItem.title = "Companies"
+        
+        tableView.backgroundColor = .darkBlue
+//        tableView.separatorStyle = .none
+        tableView.separatorColor = .white
+//        tableView.tableFooterView = UIView()
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "plus")?.withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: #selector(handleAddCompany))
         
@@ -24,10 +31,37 @@ class ViewController: UIViewController {
         navigationController?.navigationBar.scrollEdgeAppearance = newNavBarAppearance
         navigationController?.navigationBar.compactAppearance = newNavBarAppearance
         navigationController?.navigationBar.standardAppearance = newNavBarAppearance
+        
+        if #available(iOS 15.0, *) {
+            tableView.sectionHeaderTopPadding = 0
+        }
     }
     
     @objc func handleAddCompany() {
         print("Adding company..")
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView()
+        view.backgroundColor = .lightBlue
+        return view
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        cell.backgroundColor = .tealColor
+        cell.textLabel?.text = "THE COMPANY NAME"
+        cell.textLabel?.textColor = .white
+        cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 8    // arbitrary
     }
     
     @available(iOS 13.0, *)
@@ -36,8 +70,7 @@ class ViewController: UIViewController {
         
         // Apply a light red background
         appearance.configureWithOpaqueBackground()
-        let lightRed = UIColor(red: 247/255, green: 66/255, blue: 82/255, alpha: 1)
-        appearance.backgroundColor = lightRed
+        appearance.backgroundColor = .lightRed
         
         // Apply white colored normal and large titles
         appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
