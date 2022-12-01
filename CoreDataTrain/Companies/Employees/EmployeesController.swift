8e29f892caad1e10815f8bd11a21cc02ed776c9d
iOS developer K <iOSdvlprK@gmail.com>
2022-12-02 00:12:03 +0900

    list employees and tuples

---
+++ CoreDataTrain/Companies/Employees/EmployeesController.swift
@@ -0,0 +1,73 @@
+//
+//  EmployeesController.swift
+//  CoreDataTrain
+//
+//  Created by joe on 2022/11/30.
+//
+
+import UIKit
+import CoreData
+
+class EmployeesController: UITableViewController, CreateEmployeeControllerDelegate {
+    
+    func didAddEmployee(employee: Employee) {
+        employees.append(employee)
+        tableView.reloadData()
+    }
+    
+    var company: Company?
+    var employees = [Employee]()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationItem.title = company?.name
+    }
+    
+    private func fetchEmployees() {
+        print("Trying to fetch employees..")
+        
+        let context = CoreDataManager.shared.persistentContainer.viewContext
+        let request = NSFetchRequest<Employee>(entityName: "Employee")
+        do {
+            let employees = try context.fetch(request)
+//            employees.forEach { print("Employee name:", $0.name ?? "") }
+            self.employees = employees
+        } catch let err {
+            print("Failed to fetch employees:", err)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return employees.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        let employee = employees[indexPath.row]
+        cell.textLabel?.text = employee.name
+        cell.backgroundColor = .tealColor
+        cell.textLabel?.textColor = .white
+        cell.textLabel?.font = .boldSystemFont(ofSize: 15)
+        return cell
+    }
+    
+    let cellId = "cellllllllllllId"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        fetchEmployees()
+        tableView.backgroundColor = .darkBlue
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+        setupPlusButtonInNavBar(selector: #selector(handleAdd))
+    }
+    
+    @objc private func handleAdd() {
+        print("Trying to add an employee..")
+        let createEmployeeController = CreateEmployeeController()
+        createEmployeeController.delegate = self
+        let navController = UINavigationController(rootViewController: createEmployeeController)
+        navController.modalPresentationStyle = .fullScreen
+        present(navController, animated: true, completion: nil)
+    }
+}
