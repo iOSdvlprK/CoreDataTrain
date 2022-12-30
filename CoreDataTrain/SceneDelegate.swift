248e6dfcaa70c3fd443d845015a878fa2fd73706
iOS developer K <iOSdvlprK@gmail.com>
2022-12-31 02:05:25 +0900

    auto UI updates practice with NSFetchedResultsController

--- CoreDataTrain/SceneDelegate.swift
+++ CoreDataTrain/SceneDelegate.swift
@@ -45,7 +45,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let companiesController = CompaniesController()
+        let companiesController = CompaniesAutoUpdateController()
         let navController = CustomNavigationController(rootViewController: companiesController)
         window.rootViewController = navController
         self.window = window
